bf1426820625c4ce79a972019f28386d7afdc879
fhoussam <houssamfertaq@gmail.com>
2022-09-26 23:45:37 +0200

    angular local port listening issue

--- Mt.Angular/ClientApp/src/app/app.module.ts
+++ Mt.Angular/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MainModule } from './main/main.module';
 import { SharedModule } from './shared/shared.module';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,12 @@ import { EffectsModule } from '@ngrx/effects';
       { path: '', component: HomeComponent, pathMatch: 'full' },
     ]),
     StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([])
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, 
+      logOnly: environment.production, 
+      autoPause: true,
+    }),
   ],
   bootstrap: [AppComponent]
 })
